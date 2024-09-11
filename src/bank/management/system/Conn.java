02c3d77ca6421db36a9dbaad4c7974e66ee55d23
Nikitaa19 <agarwalnikita064@gmail.com>
2024-09-11 22:36:39 +0530

    fifth commit

--- src/bank/management/system/Conn.java
+++ src/bank/management/system/Conn.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package bank.management.system;
+//package bank.management.system;
 
-/**
- *
- * @author Administrator
- */
+import java.sql.*;
+
 public class Conn {
-    
+
+    Connection c;
+    Statement s;
+    public Conn() {
+        try {
+            //Class.forName(com.mysql.cj.jdbc.Driver);
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "8Vb&8Ih$");
+            s = c.createStatement();
+
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+    }
 }
