9d84cc44620824ea8897414f737adb90a73d0420
Nikitaa19 <agarwalnikita064@gmail.com>
2024-09-19 23:40:13 +0530

    pinchange update

--- src/bank/management/system/PinChange.java
+++ src/bank/management/system/PinChange.java
@@ -1,6 +1,15 @@
 package bank.management.system;
 
+import javax.swing.*;
 
-public class PinChange {
+public class PinChange extends JFrame{
+    
+    PinChange(String pinchange) {
+        ImageIcon
+    }
+    
+    public static void main(String args[]) {
+        new PinChange("").setVisible(true);
+    }
     
 }
