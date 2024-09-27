54e9413e33f37b7926dc11cb0dc58fb066807b76
Nikitaa19 <agarwalnikita064@gmail.com>
2024-09-27 19:24:57 +0530

    fourty seventh commit

--- src/bank/management/system/BalanceEnquiry.java
+++ src/bank/management/system/BalanceEnquiry.java
@@ -1,10 +1,24 @@
 package bank.management.system;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class BalanceEnquiry extends JFrame {
     
     BalanceEnquiry(String pinchange) {
+        
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 900, 900);
+        add(image);
+        
+        setSize(900, 900);
+        setLocation(300, 0);
+        setVisible(true);
         
     }
     
