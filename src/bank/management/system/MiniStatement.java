628fddf0234eb5f474b102fa6df3534bdbce037f
Nikitaa19 <agarwalnikita064@gmail.com>
2024-09-30 21:55:46 +0530

    bank labelled

--- src/bank/management/system/MiniStatement.java
+++ src/bank/management/system/MiniStatement.java
@@ -1,11 +1,21 @@
 package bank.management.system;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MiniStatement extends JFrame {
     
     MiniStatement() {
         setTitle("Mini Statement");
+        
+        setLayout(null);
+        
+        JLabel text = new JLabel();
+        add(text);
+        
+        JLabel bank = new JLabel("Indian Bank");
+        bank.setBounds(150, 20, 100, 20);
+        add(bank);
         
         setSize(400, 600);
         setLocation(20, 20);
