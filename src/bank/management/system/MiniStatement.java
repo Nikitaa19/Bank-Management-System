a08cef22c84b52a03b29b0a888dff8a3069c4e5a
Nikitaa19 <agarwalnikita064@gmail.com>
2024-09-30 21:40:13 +0530

    statement

--- src/bank/management/system/MiniStatement.java
+++ src/bank/management/system/MiniStatement.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bank.management.system;
 
-/**
- *
- * @author Administrator
- */
 public class MiniStatement {
     
 }
