ce25ecb175a0bcd47e4e56cc3f94fc5ffc5586f0
Nikitaa19 <agarwalnikita064@gmail.com>
2024-09-30 21:44:34 +0530

    constructor

--- src/bank/management/system/MiniStatement.java
+++ src/bank/management/system/MiniStatement.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 
 public class MiniStatement extends JFrame {
     
+    MiniStatement() {
+        setTitle("Mini Statement");
+    }
+    
     public static void main(String args[]) {
         new MiniStatement();
     }
