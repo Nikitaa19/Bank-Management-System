ae13d4deb3b836f878f7e531a9ccaa10b23a79a9
Nikitaa19 <agarwalnikita064@gmail.com>
2024-09-30 21:51:23 +0530

    coloured

--- src/bank/management/system/MiniStatement.java
+++ src/bank/management/system/MiniStatement.java
@@ -6,6 +6,11 @@ public class MiniStatement extends JFrame {
     
     MiniStatement() {
         setTitle("Mini Statement");
+        
+        setSize(400, 600);
+        setLocation(20, 20);
+        getContentPane().setBackground(Color.WHITE);
+        setVisible(true);
     }
     
     public static void main(String args[]) {
